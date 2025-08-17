93fd099035d7a84b3a6290cff55ef75c02cd639c
GJG <104495388+stu442@users.noreply.github.com>
2025-08-17 21:49:36 +0900

    fix: sitemap 항상 200 받도록 수정

--- next.config.js
+++ next.config.js
@@ -87,6 +87,14 @@ module.exports = () => {
           source: '/(.*)',
           headers: securityHeaders,
         },
+        {
+          source: '/sitemap.xml',
+          headers: [
+            { key: 'Cache-Control', value: 'no-cache, no-store, must-revalidate' },
+            { key: 'Pragma', value: 'no-cache' },
+            { key: 'Expires', value: '0' },
+          ],
+        },
       ]
     },
     webpack: (config, options) => {
